7ad6720e9576d7ff6c175711a2570d56f5bf6d57
Sayed Afnan Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2024-02-17 00:48:29 +0530

    cleanup

--- frontend/src/pages/Recommended.jsx
+++ frontend/src/pages/Recommended.jsx
@@ -16,15 +16,15 @@ function Recommended() {
 
   useEffect(() => {
     const getRecommended = async () => {
-      console.log(Date.now(),"Requested for page", page)
+      // console.log(Date.now(),"Requested for page", page)
       const res = await AxFetch.get(`/api/recommended/${page}`, { validateStatus: false })
-      console.log(Date.now(),"DATA RECIEVED FOR PAGE",page,"DATA:",res.data)
+      // console.log(Date.now(),"DATA RECIEVED FOR PAGE",page,"DATA:",res.data)
       return res.data;
     }
-    console.log(Date.now(),"BEGINNING REQUEST FOR NEXT PAGE", page)
+    // console.log(Date.now(),"BEGINNING REQUEST FOR NEXT PAGE", page)
     getRecommended(page).then((data) => {
         setEvents(prevEvents => [...prevEvents, ...data])
-        console.log(Date.now(),"Added events")
+        // console.log(Date.now(),"Added events")
         setContinuousLoad(false)
     })
   }, [page])
@@ -33,9 +33,9 @@ function Recommended() {
   const handleScroll = (event) => {
     const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
     const scrollPercentage = (scrollTop + clientHeight) / scrollHeight;
-    console.log("HANDLE SCROLL HAS BEEN ACTIVATED!!")
+    // console.log("HANDLE SCROLL HAS BEEN ACTIVATED!!")
     if (scrollPercentage > 0.90) { 
-      console.log("Reached bottom")
+      // console.log("Reached bottom")
       setContinuousLoad(true)
       setPage(prevPage => prevPage + 1);
     }
@@ -46,7 +46,7 @@ function Recommended() {
       {!isLoadingEvents && events.length > 0 ? <div className="container mx-auto p-4">
         <h1 className="text-3xl text-center font-bold mt-8 mb-12">{state?.user ? "Recommended Events For You" : "Events"}</h1>
         <div onScroll={handleScroll} style={{height:'70vh'}} className="grid gap-9 md:grid-cols-2 lg:grid-cols-3 items-start overflow-auto">
-          { console.log("[COMPONENT RELOAD OCCURRED] Total events displayed now:",events.length) }
+          {/* { console.log("[COMPONENT RELOAD OCCURRED] Total events displayed now:",events.length) } */}
           {events?.map((event, index) => (
             <EventCard key={index} event={event} />
           ))}
